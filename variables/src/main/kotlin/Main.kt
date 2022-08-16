b4f544eef88736ae3510f91ec027a5e87f396878
jrivera63 <89046450+jrivera63@users.noreply.github.com>
2022-08-16 17:39:23 -0500

    Add files via upload

---
+++ variables/src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+// This program utilizes different variable types and displays them
+
+fun main(args: Array<String>) {
+    // byte
+    val myByte: Byte = 17
+    println("Byte = $myByte")
+
+    //short
+    val myShort : Short = 10000
+    println("Short = $myShort")
+
+    //integer
+    val myInt: Int = 2000000
+    println("Int = $myInt")
+
+    //long
+    val myLong: Long = 50000000L
+    println("Long = $myLong")
+
+    //float
+    val myFloat: Float = 2.55F
+    println("Float = $myFloat")
+
+    //double
+    val myDouble: Double = 200.42435443242
+    println("Double = $myDouble")
+
+    //boolean
+    val myBoolean: Boolean = true
+    println("Boolean = $myBoolean")
+
+    //char
+    val myChar: Char = 'j'
+    println("Char = $myChar")
+
+
+}
