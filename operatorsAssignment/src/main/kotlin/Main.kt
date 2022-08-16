2196dfa27d648330d91d17b4dc21f617865bf70a
jrivera63 <89046450+jrivera63@users.noreply.github.com>
2022-08-16 04:42:55 -0500

    Add files via upload

---
+++ operatorsAssignment/src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+// Program that utilizes different operators and shows the results
+
+fun main(){
+    val a = 20
+    val b = 31
+    val answer1 = a + b
+    println("20 + 31 = " + answer1)
+
+    val c = 20
+    val d = 4
+    val answer2 = c / d
+    println("20 / 4 = " + answer2)
+
+    val e = 30
+    val f = 11
+    val answer3 = e % f
+    println("30 % 11 = " + answer3)
+
+    val g = 59
+    val h = 2
+    println("59 != 2 is " + (g != h))
+
+    var i = 6
+    i--
+    println("6-- = " + i)
+}
+
